79e1b4c2b3a465b361813027e57ea26cb82cba78
Ugolin O. <hello@ugolin-olle.com>
2025-04-15 09:25:32 +0200

    feat(nvim): update snacks configuration

--- lua/plugins/editor/snacks.lua
+++ lua/plugins/editor/snacks.lua
@@ -8,10 +8,10 @@ return {
   opts = {
     dashboard = {
       width = 60,
-      row = nil,                                                                   -- dashboard position. nil for center
-      col = nil,                                                                   -- dashboard position. nil for center
-      pane_gap = 4,                                                                -- empty columns between vertical panes
-      autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
+      row = nil,
+      col = nil,
+      pane_gap = 4,
+      autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
       preset = {
         pick = nil,
         keys = {
@@ -19,7 +19,11 @@ return {
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
